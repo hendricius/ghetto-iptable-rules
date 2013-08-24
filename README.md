6661ab4fe8de09d0897da0e2767486d1f0ef40bb
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-08-24 12:37:19 +0200

    simple iptable rules

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic ghetto iptable rules
+
+    ./drop_iptable_rules.sh
+
+Then
+
+    ./add_iptable_rules.sh
+
+
+See
+
+    add_iptable_rules.sh
+
+for some examples/useful configurations.
---
+++ add_iptable_rules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+/sbin/iptables -A INPUT -i lo -j ACCEPT
+/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# SSH
+/sbin/iptables -A INPUT -p tcp -m tcp --dport ssh -j ACCEPT
+
+# Example to allow accessing a local port via ssh
+# /sbin/iptables -A INPUT -p tcp -s 127.0.0.1 -m tcp --dport 10000 -j ACCEPT
+# Then log in via ssh: ssh x@x.com -L 10000:localhost:10000
+/sbin/iptables -A INPUT -p tcp -m tcp --dport https -j ACCEPT
+/sbin/iptables -A INPUT -p tcp -m tcp --dport http -j ACCEPT
+
+# Rails
+/sbin/iptables -A INPUT -p tcp -m tcp --dport 3000 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp -m tcp --dport 3001 -j ACCEPT
+
+# VPN, for running a openvpn server
+# /sbin/iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT
+# /sbin/iptables -A OUTPUT -o eth0 -p udp --dport 1194 -j ACCEPT
+# /sbin/iptables -A INPUT -i tun0 -j ACCEPT
+# /sbin/iptables -A OUTPUT -o tun0 -j ACCEPT
+# /sbin/iptables -A FORWARD -o tun0 -j ACCEPT
+
+/sbin/iptables -P INPUT DROP
+/sbin/iptables -P FORWARD DROP
+/sbin/iptables -P OUTPUT ACCEPT
